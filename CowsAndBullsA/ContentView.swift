0daf71a994378160bdbf1e30b543bb7830ad384e
superkookai <weerawut@live.com>
2025-12-30 08:11:54 +0700

    Initial Commit

---
+++ CowsAndBullsA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CowsAndBullsA
+//
+//  Created by Weerawut on 30/12/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CowsAndBullsA/CowsAndBullsAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsAApp.swift
+//  CowsAndBullsA
+//
+//  Created by Weerawut on 30/12/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
